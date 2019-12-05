a5fc9043a213369fba495ab309b2ed7e90f7542f
Raffael <github@raffaeljesche.de>
2019-12-05 11:23:02 +0100

    added drag handle icon, css fix for empty areas

--- views/dashboard.php
+++ views/dashboard.php
@@ -17,10 +17,11 @@
         @trigger('admin.dashboardgrid.widgets.top', compact('widgets', 'areas'))
 
         <div class="uk-width-medium-1-2" data-area="main">
-            <div class="uk-sortable uk-grid uk-grid-gutter uk-grid-width-1-1" data-uk-sortable="{group:'dashboard',animation:false}">
+            <div class="uk-sortable uk-grid uk-grid-gutter uk-grid-width-1-1" data-uk-sortable="{group:'dashboard',animation:false,handleClass:'dashboard-handle'}">
               @if(isset($areas['main']))
                 @foreach($areas['main'] as $widget)
                 <div data-widget="{{ $widget['name'] }}">
+                    <i class="dashboard-handle uk-icon-arrows"></i>
                     {{ $widget['content'] }}
                 </div>
                 @endforeach
@@ -28,10 +29,11 @@
             </div>
         </div>
         <div class="uk-width-medium-1-4" data-area="aside-left">
-            <div class="uk-sortable uk-grid uk-grid-gutter uk-grid-width-medium-1-1" data-uk-sortable="{group:'dashboard',animation:false}">
+            <div class="uk-sortable uk-grid uk-grid-gutter uk-grid-width-medium-1-1" data-uk-sortable="{group:'dashboard',animation:false,handleClass:'dashboard-handle'}">
               @if(isset($areas['aside-left']))
                 @foreach($areas['aside-left'] as $widget)
                 <div data-widget="{{ $widget['name'] }}">
+                    <i class="dashboard-handle uk-icon-arrows"></i>
                     {{ $widget['content'] }}
                 </div>
                 @endforeach
@@ -39,10 +41,11 @@
             </div>
         </div>
         <div class="uk-width-medium-1-4" data-area="aside-right">
-            <div class="uk-sortable uk-grid uk-grid-gutter uk-grid-width-medium-1-1" data-uk-sortable="{group:'dashboard',animation:false}">
+            <div class="uk-sortable uk-grid uk-grid-gutter uk-grid-width-medium-1-1" data-uk-sortable="{group:'dashboard',animation:false,handleClass:'dashboard-handle'}">
               @if(isset($areas['aside-right']))
                 @foreach($areas['aside-right'] as $widget)
                 <div data-widget="{{ $widget['name'] }}">
+                    <i class="dashboard-handle uk-icon-arrows"></i>
                     {{ $widget['content'] }}
                 </div>
                 @endforeach
@@ -60,7 +63,7 @@
             <div class="uk-grid" data-uk-grid-margin>
 
                 <div class="uk-width-9-10">
-                    <div class="uk-sortable uk-grid uk-grid-gutter uk-width-1-1 uk-grid-width-medium-1-3 uk-grid-width-large-1-5" data-uk-sortable="{group:'dashboard',animation:false}">
+                    <div class="uk-sortable uk-grid uk-grid-gutter uk-width-1-1 uk-grid-width-medium-1-3 uk-grid-width-large-1-5" data-uk-sortable="{group:'dashboard',animation:false,handleClass:'dashboard-handle'}">
                       @if(isset($areas['trash']))
                         @foreach($areas['trash'] as $widget)
                         <div data-widget="{{ $widget['name'] }}">
@@ -87,9 +90,6 @@
 
 <style>
 
-    /* #dashboard .uk-grid.uk-sortable {
-        min-height: 10vh;
-    } */
     .dashboard-display-grid:nth-child(even) {
         background-color: rgba(0,0,0,.1);
     }
@@ -97,12 +97,13 @@
         background-color: rgba(0,0,0,.05);
     }
     .uk-sortable-item:not(.uk-sortable-placeholder) > div {
-        /* box-shadow: 0 34px rgba(0,90,255,.2), 0 -34px rgba(0,90,255,.2); */
         box-shadow: 0 34px rgba(0,90,255,.2), 0 -34px rgba(0,90,255,.2), 34px 0 rgba(0,90,255,.2), -34px 0 rgba(0,90,255,.2);
     }
-    /* .dashboard-trash.dashboard-display-grid { */
-    .dashboard-display-grid {
-        min-height: 10vh;
+    #dashboard .uk-sortable-empty {
+        min-height: unset;
+    }
+    .dashboard-display-grid, #dashboard .dashboard-display-grid .uk-sortable-empty {
+        min-height: 40px;
     }
     .dashboard-trash [data-widget] {
         display: none;
@@ -111,7 +112,30 @@
         opacity: .5;
         display: block;
     }
-    
+    #dashboard [data-widget] {
+        position: relative;
+    }
+    #dashboard .dashboard-handle {
+        position: absolute;
+        top: -.7em;
+        left: 50%;
+        display: none;
+        z-index: 1;
+        padding: .2em;
+        color: #dfdfdf;
+    }
+    #dashboard .dashboard-handle:hover {
+        font-size: 1.2em;
+        background-color: #fff;
+        color: #222;
+        border: 1px solid #ccc;
+        border-radius: 50%;
+        cursor: move;
+        margin-left: -.1em;
+    }
+    #dashboard [data-widget]:hover .dashboard-handle {
+        display: block;
+    }
 
 </style>
 
