242ba097b3b2d8d871710ad8b52fe43802b9d75c
Raffael <github@raffaeljesche.de>
2020-08-15 18:37:54 +0200

    bump version to 0.1.1

--- bootstrap.php
+++ bootstrap.php
@@ -5,11 +5,11 @@
  * @see       https://github.com/raffaelj/cockpit_DashboardGrid/
  * @see       https://github.com/agentejo/cockpit/
  * 
- * @version   0.1.0
+ * @version   0.1.1
  * @author    Raffael Jesche
  * @license   MIT
  */
 
 if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
-    include(__DIR__ . '/admin.php');
+    include_once(__DIR__ . '/admin.php');
 }
